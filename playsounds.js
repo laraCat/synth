60fd082ec88e480502f1429c6f6b736f9d9c0b7a
Lara <heavyhearts@gmail.com>
2015-10-23 12:55:50 -0700

    Update playsounds.js

--- playsounds.js
+++ playsounds.js
@@ -13,6 +13,7 @@ var BD;
 T("audio").load("./drumkit.wav", function() {
 drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD).play()
 BD = this.slice(0, 500).set({bang:false})
+console.log('file loaded')
 })
 var socket = io.connect('http://192.168.1.4:3042')
 socket.on('connect', function () {
