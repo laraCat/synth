4681b8b6eb1c9ab7d94624ac0768e718e680098d
Lara <heavyhearts@gmail.com>
2015-10-23 12:56:08 -0700

    Update playsounds.js

--- playsounds.js
+++ playsounds.js
@@ -15,7 +15,7 @@ drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD).play()
 BD = this.slice(0, 500).set({bang:false})
 console.log('file loaded')
 })
-var socket = io.connect('http://192.168.1.4:3042')
+var socket = io.connect('http://192.168.1.4:3043')
 socket.on('connect', function () {
 console.log('connected to socket')
 })
