2c0dd5032711b7a1ecee6cb39434b3e705eaf15b
Lara <heavyhearts@gmail.com>
2015-10-27 11:28:45 -0700

    Update playsounds.js

--- js/playsounds.js
+++ js/playsounds.js
@@ -1,30 +1,29 @@
 
+
 var socket = io.connect('http://192.168.1.2:8080')
 socket.on('connect', function () {
 console.log('connected to socket')
 })
 
-//var sin = T("sin", {freq:440, mul:0.5}); 
+var v = 0; //declare variable for pot values
+var sin = T("sin", {freq:v, mul:0.5}); //change frequency of sine wave with pot values 
 
-socket.on('button', function (value) { // button is pushed, play this file
-if (value == 'push') {
-T("audio").loadthis("drumkit.wav", function() {
- this.play();
-})
-
-.on("ended", function() {
-  this.pause();
-});	
-
+socket.on('button', function (value) { 
+if (value == 'push') { 
+//T("audio").loadthis("drumkit.wav", function() { //play file
+// this.play();
+//})
+sin.play(); //play sine wave
 }
-
+//if (value == 'release'){ 
+//sin.pause(); //pause sine wave
+//}
 })
 
-var v = 0;
-socket.on('analogSensor', function (value) {
-		v = value
-	T("delay", {time:v, fb:0.4, mix:0.25}, this).play();
-		
+
+socket.on('analog', function (value) {
+		v = value // put pot values in variable
 	}) 
+
 
 
